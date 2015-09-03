30c71d77dc3e627083aeae99904c72880ada8746
gchen <gchen@rsmas.miami.edu>
2015-09-03 18:56:58 -0400

    update food chicken basil

--- scripts/pyscript/myATT_monthly_est.py
+++ scripts/pyscript/myATT_monthly_est.py
@@ -9,21 +9,21 @@ import pdb
 
 import numpy as np
 
-def myATT_monthly_est(actualPayment,data,nMembers,otherFees):
+def myATT_monthly_est(members, actualPayment,data,nMembers,otherFees):
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #%    individual charge
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     talk = 9.99; #% do not discount, mothly charge for individual members
     if not data: #% an empty data should be the default
-        data = np.array([25.0,15.0,0.0]); #% please discount
+        data = np.array([25.0, 15.0, 0.0, 20]); #% please discount
 	# pdb.set_trace()
-    print '\nNOTE: Default Data: Hsiaochu($25), Ginos($15), Jinghao($0)\n'
+    print '\nNOTE: Default Data: Hsiaochu($25), Ginos($15), Jinghao($0), Xia($20)\n'
     nMembers = len(data); #% Members include Hsiaochu (305-299-7934), Gino (305-299-8134), and Jinghao (305-755-7691) 
 
     #shared = np.around(shared_total/nMembers,decimals=2); #% please discount. (The monthly_charge of $50 in 305-2998134 account includes monthly_charge).
     if not otherFees:
-        otherFees = np.array([4.48,4.48,4.48]);  #% do not discount, other_fees ($4.11) should be the default
-    print 'NOTE: Default Fees: Hsiaochu($4.11), Ginos($4.11), Jinghao($4.11)\n'
+        otherFees = np.array([4.48,4.48,4.48,4.48]);  #% do not discount, other_fees ($4.11) should be the default
+    print 'NOTE: Default Fees: Hsiaochu($4.48), Ginos($4.48), Jinghao($4.48)\n'
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #%    other variable
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -32,8 +32,8 @@ def myATT_monthly_est(actualPayment,data,nMembers,otherFees):
     noshare = talk + data*payRatio + otherFees;
     shared = (actualPayment - np.sum(noshare,axis=0))/nMembers; # this way includes the monthly 700 min plan plus the extra fees
     individualPayments = noshare + shared;
-
-    members = ['Hsiaochu','Gino','Jinghao'];
+    print shared
+#    members = ['Hsiaochu','Gino','Jinghao'];
     print 'Individual Payments = talk + data*payRatio + shared + otherFees:\n'
     for i in range(nMembers): # 0 <= i < nMembers
         print '      {5} = ${0} + ${1}*{3} + ${2} + ${4} = ${6} \n' \
@@ -45,5 +45,9 @@ def myATT_monthly_est(actualPayment,data,nMembers,otherFees):
 # Apply the function myATT_monthly_est in the main.py program
 if __name__ == "__main__":
     import sys
-    (actualPayment,estPayment) = myATT_monthly_est(124,[],3,[4.71,4.48,4.48]);
+    members = ['Hsiaochu','Gino','Jinghao','Xia'];
+    actualPay = 165.26;
+    nmembers = 4;
+    otherFees = [4.92, 4.48, 5.15, 10.65];
+    (actualPayment,estPayment) = myATT_monthly_est(members, actualPay, [], nmembers, otherFees);
     print 'Estimated Payment (${}) == Actual Payment (${}) ?\n'.format(estPayment,actualPayment) # sum over the rows (axis=0)
