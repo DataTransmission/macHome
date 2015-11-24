bd21b29398af17ac8c592464168c216531ec0923
gino_macbookair <gchen@rsmas.miami.edu>
2015-11-23 22:59:52 -0500

    use SPCAM!!!

--- scripts/matscript/CAMSAM_exp/area_ave.m
+++ scripts/matscript/CAMSAM_exp/area_ave.m
@@ -4,7 +4,7 @@ lev = ncread(ncfile_CAM,'lev')
 lat = ncread(ncfile_CAM,'lat')
 lon = ncread(ncfile_CAM,'lon')
 varname = 'U' % V, W
-ncfile = [varname '_sum.nc'];
+ncfile = [varname '_tav.nc'];
 dimname = {'x','y','z','time','p'}
 for id = 1:numel(dimname) 
    eval(sprintf('%s = ncread(ncfile,dimname{id});',dimname{id})); 
@@ -12,12 +12,12 @@ end
 nx = numel(x); % nx = 5120; x_crm = 20480000m; earth circumference = 39690000m
 ny = numel(y); % ny = 2560; y_crm = 10240000m;
 nz = numel(z);
-nt = 240; % number of time samples 2592000/10800 = 240 = 30 days
+nt = 1; % number of time samples 2592000/10800 = 240 = 30 days
 nbox_x = 32; % nbox_x of 4km boxes adds up to nbox_x*4 = 128km approximately 1.16 degrees
 nbox_y = 32; 
 var = ncread(ncfile,varname); % time-summed var
-var = squeeze(var(1,:,:,:))/nt; % average of the time-summed var
 % var(time, z, y, x)
+var = squeeze(var(1,:,:,:))/nt; % average of the time-summed var
 %
 % CRM xy average (xy bar)
 % Purpose : treat turbulence to be locally deviated from local box
@@ -45,3 +45,9 @@ for iz = 1:nz
       jj = jj + 1;
    end
 end
+%
+% TKE = xymean(u'^2 + v'^2 + w'^2)/2
+% TPERT : Perturbation temperature (eddies in PBL)
+% PBLH 
+%
+
